a65982b23fd3c3c63e4468455ace8b51214009d1
Phatthawut <42006909+Phatthawut@users.noreply.github.com>
2024-05-15 10:13:37 +0700

    Create script for function exercise 1

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a*b;
+}
+
+const divide = function(a ,b) {
+    return a/b;
+}
+
+console.log(add(3,6));
+console.log(subtract(45,36));
+console.log(multiply(6,4));
+console.log(divide(16,2));
